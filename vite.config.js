13e9f7d72e918b7490d232e3e6c3fedca6a95e98
Pukkok <krystalgggg@gmail.com>
2025-06-14 20:59:38 +0900

    [Fix] vite 설정으로 전체 변경

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'), // 선택: 절대경로 import 지원
+  //   },
+  // },
+  server: {
+    port: 5454,
+  },
+})
