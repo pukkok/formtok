226644d063bc2399f0e3c97db82b48b11a24f0f9
Pukkok <krystalgggg@gmail.com>
2025-06-15 22:33:12 +0900

    [Fix] vite 사용 .env설정 변경

--- src/C-Hooks/useAxios.js
+++ src/C-Hooks/useAxios.js
@@ -3,7 +3,7 @@ import { useRecoilValue, useSetRecoilState } from "recoil"
 import { originalDataAtom, pagesAtom, randomKey } from "../C-Recoils/surveyAtoms"
 import { useCallback } from "react"
 
-axios.defaults.baseURL = origin.includes('localhost') ? `http://localhost:5000` : process.env.REACT_APP_RESTAPI_URL
+axios.defaults.baseURL = import.meta.env.VITE_PUBLIC_RESTAPI_URL
 
 const useAxios = () => {
     const pages = useRecoilValue(pagesAtom)
