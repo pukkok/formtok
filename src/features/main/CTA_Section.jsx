5137e343550ff4c1b4d57b027a7f18278d3da281
Pukkok <krystalgggg@gmail.com>
2025-06-15 22:32:10 +0900

    [Fix] CTA -> PageSwitchButton으로 변경

--- src/features/main/CTA_Section.jsx
+++ src/features/main/CTA_Section.jsx
@@ -1,4 +1,4 @@
-import NavigateButton from '../../A-Components/Buttons/NavigateButton'
+import PageSwitchButton from '../../A-Components/PageSwitch/PageSwitchButton'
 
 const CONTENT = {
   headLine : '한번 해보는게 빠르죠!',
@@ -13,11 +13,11 @@ const CTA_Section = () => {
     <section className='py-20 px-10 flex flex-col items-center bg-deep-dark'>
       <h3 className='text-3xl mb-5 text-point'>{CONTENT.headLine}</h3>
       <p className='text-lg mb-10'>{CONTENT.content}</p>
-      <NavigateButton 
+      <PageSwitchButton 
         className={'px-6 py-3 font-bold text-lg bg-point rounded-lg transition-[background-color] duration-300 hover:bg-point-hover'}
         to={CONTENT.navigate}>
         {CONTENT.navText}
-      </NavigateButton>
+      </PageSwitchButton>
     </section>
   )
 }
