dcdffde42c554c304252b1435f6d4236b506433f
Pukkok <krystalgggg@gmail.com>
2025-06-15 22:26:42 +0900

    [Feat] ImgBox 테일윈드 스타일로 변경

--- src/A-Components/ImgBox.jsx
+++ src/A-Components/ImgBox.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-
-function ImgBox ({className, boxStyle, src, alt="", draggable=false}) {
+const ImgBox = ({className, boxStyle, src, alt="", draggable=false}) => {
 
   return (
-    <StyledImgBox className={className} style={boxStyle}>
-      <img src={src} alt={alt} draggable={draggable}/>
-    </StyledImgBox>
+    <div className={`overflow-hidden ${className}`} style={boxStyle}>
+      <img className="w-full h-full" src={src} alt={alt} draggable={draggable}/>
+    </div>
   )
 }
 
 export default ImgBox
-
-const StyledImgBox = styled.div`
-  overflow: hidden;
-`
