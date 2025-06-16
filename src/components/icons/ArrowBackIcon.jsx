d29612f1c3b785a5a583b86aeb97a37a4a67a2e4
Pukkok <krystalgggg@gmail.com>
2025-06-17 00:18:58 +0900

    [Feat] svg 아이콘 생성

---
+++ src/components/icons/ArrowBackIcon.jsx
@@ -0,0 +1,18 @@
+export const ArrowBackIcon = ({ size = 24, className = '', ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+    {...props}
+  >
+    <path d="M19 12H5" />
+    <path d="M12 19l-7-7 7-7" />
+  </svg>
+)
