e5958b8de2e00765c919ce0e9697600fa2ea14e1
Pukkok <krystalgggg@gmail.com>
2025-06-17 00:19:39 +0900

    [Fix] 네비게이션 버튼 경로 옵션 수정

--- src/A-Components/Buttons/NavigateButton.jsx
+++ src/A-Components/Buttons/NavigateButton.jsx
@@ -1,21 +1,36 @@
 'use client'
 
-// import { useNavigate } from "react-router-dom";
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-function NavigateButton ({ to='/', handleClick = null, className, children }) {
+function NavigateButton({ to = '/', handleClick = null, className, children }) {
   const router = useRouter()
-  // const navigate = useNavigate()
+  const [canGoBack, setCanGoBack] = useState(false)
 
-  const onClick = (to) => {
-    router.push(to)
-    if(handleClick) handleClick()
+  useEffect(() => {
+    // INFO: 최소한 2 이상이면 back할 히스토리가 있음
+    setCanGoBack(window.history.length > 2)
+  }, [])
+
+  const onClick = () => {
+    if (to === 'back') {
+      if (canGoBack) {
+        router.back()
+      } else {
+        router.push('/') // INFO: 앱 루트로 대체
+      }
+    } else {
+      router.push(to)
+    }
+
+    if (handleClick) handleClick()
   }
 
-  return <button type="button"
-    className={className}
-    onClick={()=>onClick(to)}
-    >{children}</button>
+  return (
+    <button type="button" className={className} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
 
 export default NavigateButton
