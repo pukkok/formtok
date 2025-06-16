9f77116a840a6853b1b27dc0617853737b51e7d6
Pukkok <krystalgggg@gmail.com>
2025-06-17 00:20:43 +0900

    [Fix] 페이지 root에 pageSwticher 가져오기

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
+import PageSwitcher from "@/A-Components/PageSwitch/PageSwitcher";
 import "./index.css"
+import { Toaster } from "sonner";
 
 export const metadata = {
   title: "폼톡"
@@ -8,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <PageSwitcher />
         {children}
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
