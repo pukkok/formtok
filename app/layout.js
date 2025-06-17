d1b2f4837b43d60de0d3f2797af78aae66956245
Pukkok <krystalgggg@gmail.com>
2025-06-18 00:44:35 +0900

    [Feat] 테마 옵저버 연결

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-import PageSwitcher from "@/A-Components/PageSwitch/PageSwitcher";
+import PageSwitcher from "@/components/page-switch/PageSwitcher";
 import "./index.css"
 import { Toaster } from "sonner";
+import ThemeObserver from "./ThemeObserver";
 
 export const metadata = {
   title: "폼톡"
@@ -10,10 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <PageSwitcher />
-        {children}
-        <Toaster richColors position="top-center" />
+        <ThemeObserver>
+          <PageSwitcher />
+          {children}
+          <Toaster richColors position="top-center" />
+        </ThemeObserver>
       </body>
     </html>
-  );
+  )
 }
