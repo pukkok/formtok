e01c8bc2ccf1be996591696fb444ae62582d71b3
Pukkok <krystalgggg@gmail.com>
2025-06-18 17:47:23 +0900

    [Feat] 사이드바 네비게이션 생성

---
+++ src/features/form-sidebar/compoents/SidebarNavs.jsx
@@ -0,0 +1,30 @@
+import { HomeIcon, MyFormIcon, EidtIcon, ResultIcon, QuestionListIcon, FormListIcon, DashBoardIcon, SettingIcon } from "@/A-Components/Icons/Icons"
+import TreeNode from "./TreeNode";
+
+const sidebarNavs = [
+  { text: '홈', icon: <HomeIcon />, path: '/' },
+  { 
+    text: '내 설문지', icon: <MyFormIcon />,
+    children: [
+      { text: '제작하기', icon: <EidtIcon />, path: '/my-form/manage' },
+      { text: '결과확인', icon: <ResultIcon />, path: '/my-form/result' },
+      { text: '문항관리', icon: <QuestionListIcon />, path: '/my-form/questions' }
+    ]
+  },
+  { text: '참여하기', icon: <FormListIcon />, path: '/form-list' },
+  { text: '대시보드', icon: <DashBoardIcon />, path: '/dashboard' },
+  { text: '설정', icon: <SettingIcon />, path: '/setting' },
+]
+
+const SidebarNavs = () => {
+
+  return (
+    <ul className="flex flex-col">
+      {sidebarNavs.map((node, idx) => {
+        return <TreeNode key={idx} node={node}/>
+      })}
+    </ul>
+  )
+}
+
+export default SidebarNavs
