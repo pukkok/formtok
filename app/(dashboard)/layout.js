8c9805c1b20a7b0e64a094772436603bb83bf232
Pukkok <krystalgggg@gmail.com>
2025-06-18 21:22:30 +0900

    [Fix] 레이아웃 다크모드 기본텍스트 색상 추가

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -17,7 +17,7 @@ const DashboardLayout = ({children}) => {
   return (
     <main className="flex">
       <Sidebar />
-      <div className="dark:bg-deep-dark bg-[#F1F1F1] px-10 py-5 flex-1 max-h-screen overflow-y-auto">
+      <div className="dark:bg-deep-dark dark:text-light-w bg-[#F1F1F1] px-10 py-5 flex-1 max-h-screen overflow-y-auto">
         {children}
       </div>
     </main>
