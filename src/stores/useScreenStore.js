5683a409d7d8d508214ebee4fe8e6e46b30174ab
Pukkok <krystalgggg@gmail.com>
2025-06-18 17:48:39 +0900

    [Fix] 스크린 상태관리 acitve 관리 추가

--- src/stores/useScreenStore.js
+++ src/stores/useScreenStore.js
@@ -2,13 +2,18 @@ import { create } from "zustand";
 
 export const useScreenStore = create((set, get) => ({
   mode: 'dark',
-  sidebarWidth: 320,
-  isSidebarOpen: true,
-  switchTheScreen: '',
+  setMode: (value) => set({mode : value}),
 
-  setMode: (value) => {
-    set({mode : value})
-  },
+  sidebarWidth: 320,
+
+  activeTab: '/my-form/manage',
+  setActiveTab: (value) => set({ activeTab : value }),
+  resetActiveTab: () => set({activeTab : '/my-form/manage' }),
+
+  isSidebarOpen: true,
+  setIsSidebarOpen: (boolean = true) => set({ isSidebarOpen : boolean }),
+
+  switchTheScreen: '',
 
   setSwitchTheScreen: (value) => {
     set({switchTheScreen : value})
@@ -18,11 +23,6 @@ export const useScreenStore = create((set, get) => ({
     set({ sidebarWidth : width })
   },
 
-  /** 
-   * @param {Boolean} boolean 
-   */
-  setIsSidebarOpen: (boolean = true) => {
-    set({isSidebarOpen : boolean})
-  }
+  
 
 }))
