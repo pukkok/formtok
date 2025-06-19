c2ee854fdd68d4929c152745fb08c2b9d11af0c6
Pukkok <krystalgggg@gmail.com>
2025-06-19 20:25:40 +0900

    [Feat] 검색기능 컴포넌트 구성 완료

---
+++ src/components/SearchFilter.jsx
@@ -0,0 +1,34 @@
+'use client'
+
+
+const SearchFilter = ({ filters = [], filtering = null, colorMap, hoverMap, pick = 'all' }) => {
+
+  return (
+    <div className="flex gap-2 mt-4">
+      {filters.map((filter) => {
+        const { work, text } = filter
+        const picked = pick === work
+
+        const bgColor = picked ? colorMap[work] : 'bg-white dark:bg-dark'
+        const hoverColor = picked ? '' : hoverMap[work]
+
+        return (
+          <button 
+            key={text}
+            className={`
+              ${picked ? 'text-white' : 'text-dark dark:text-white'} ${bgColor} ${hoverColor}
+              px-4 py-2 rounded-xl
+              shadow-md cursor-pointer text-sm transition-[background-color_0.3s,color_0.3s]
+              hover:text-white
+            `}
+            onClick={() => filtering(work)}
+          >
+            {text}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default SearchFilter
