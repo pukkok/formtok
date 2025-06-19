8862a2b961f1013d3caef0530e94fc6ca9e60f3b
Pukkok <krystalgggg@gmail.com>
2025-06-19 20:29:05 +0900

    [Fix] 에딧 페이지만 padding조절

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -2,22 +2,27 @@
 
 import Sidebar from "@/features/form-sidebar/Sidebar"
 import { useScreenStore } from "@/stores/useScreenStore"
+import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 
 const DashboardLayout = ({children}) => {
-
+  const pathname = usePathname()
   const resetActiveTab = useScreenStore(s => s.resetActiveTab)
 
   useEffect(() => {
-
     // INFO: 나갈 때 active 위치 초기화
     return () => resetActiveTab()
   },[resetActiveTab])
 
+  const isEditPage = pathname.includes('/edit')
+
   return (
     <main className="flex">
       <Sidebar />
-      <div className="dark:bg-deep-dark dark:text-light-w bg-[#F1F1F1] px-10 py-5 flex-1 max-h-screen overflow-y-auto">
+      <div className={`dark:bg-deep-dark dark:text-light-w bg-[#F1F1F1] 
+      flex-1 max-h-screen overflow-y-auto
+      ${isEditPage ? '' : 'px-10 py-5'}
+      `}>
         {children}
       </div>
     </main>
