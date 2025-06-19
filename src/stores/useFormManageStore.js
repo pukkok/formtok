2819e5a9e0354edf09a896a487c80af4e9c87d7a
Pukkok <krystalgggg@gmail.com>
2025-06-19 18:01:58 +0900

    [Feat] FormManage 상태관리 생성

---
+++ src/stores/useFormManageStore.js
@@ -0,0 +1,44 @@
+import { copyForm, deleteForm, getMyFormList } from "@/apis/forms";
+import { safeRequest } from "@/utils/safeRequest";
+import { create } from "zustand";
+
+export const useFormManageStore = create((set, get) => ({
+  allForms: [],
+  searchedForms: [],
+
+  getMyFormListAction : async () => {
+    const { result } = await safeRequest(getMyFormList(), {
+      loadingMessage: '설문지 불러오는 중...'
+    })
+    if(result) {
+      set({searchedForms : [...result.forms], allForms: [...result.forms]})
+      return result.forms
+    } else {
+      return set({ searchedForms: [], allForms: [] })
+    }
+  },
+
+  copyFormAction: async (url) => {
+    const { result } = await safeRequest(copyForm(url), {
+      successMessage: '설문지 복사가 완료되었습니다.',
+      onError: '설문지 복사에 실패했습니다.'
+    })
+
+    if(result) {
+      get().getMyFormListAction()
+      return true
+    }
+  },
+
+  deleteFormAction: async (url) => {
+    const { result } = await safeRequest(deleteForm(url), {
+      successMessage:'설문지 삭제가 완료되었습니다.',
+      onError: '설문지 삭제에 실패했습니다.'
+    })
+
+    if(result) {
+      get().getMyFormListAction()
+      return true
+    }
+  }
+}))
