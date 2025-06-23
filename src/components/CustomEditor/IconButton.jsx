93b6d4a46d4fb357927c90df4a4593dd3d3dd676
Pukkok <krystalgggg@gmail.com>
2025-06-23 19:31:15 +0900

    [Feat] 아이콘 버튼 isActive 추가

--- src/components/CustomEditor/IconButton.jsx
+++ src/components/CustomEditor/IconButton.jsx
@@ -1,8 +1,11 @@
-const IconButton = ({onClick, children}) => {
+const IconButton = ({onClick, isActive=false, children}) => {
 
   return (
     <button 
-      className="p-1 hover:bg-light-purple hover:text-bright-a rounded-sm"
+      className={`
+        ${isActive ? 'bg-point-hover text-bright-a' : 'hover:bg-light-purple hover:text-bright-a'} 
+        p-1 rounded-sm
+      `}
       onClick={onClick}
       >
       {children}
