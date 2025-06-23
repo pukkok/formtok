4a59103df781f1d6d4272af9d840f1223c50291f
Pukkok <krystalgggg@gmail.com>
2025-06-23 23:04:52 +0900

    [Feat] 링크 삽입 모달 생성

---
+++ src/components/CustomEditor/LinkModal.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react"
+import ModalCover from "../ModalCover"
+
+const LinkModal = ({ isOpen=false, onClose, onConfirm }) => {
+  const [url, setUrl] = useState('')
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    if (isOpen) {
+      setUrl('')
+      inputRef.current?.focus()
+    }
+  }, [isOpen])
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Escape' && onClose) onClose()
+  }
+
+  if(!isOpen) return null
+
+  return (
+    <ModalCover>
+      <form 
+        onKeyDown={handleKeyDown}
+        className="p-5 min-w-[500px] rounded-xl border-2 border-point dark:bg-deep-dark bg-light-w"
+        onSubmit={(e) => {
+          e.preventDefault()
+          if(onConfirm) onConfirm(url)
+          onClose()
+        }}
+      >
+        <h2 className="text-lg mb-2 font-semibold">링크 입력</h2>
+        <input
+          ref={inputRef}
+          type="text"
+          className="w-full p-2 border rounded"
+          placeholder="URL을 입력하세요"
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+        />
+
+        <div className="mt-4 flex justify-end gap-2">
+          <button 
+            type="button"
+            className="rounded-lg px-3 py-2 text-light-w font-bold bg-gray-500 hover:bg-gray-400 cursor-pointer" 
+            onClick={onClose}
+          >취소</button>
+          <button
+            type="submit"
+            className="rounded-lg px-3 py-2 text-light-w font-bold bg-point hover:bg-point-hover cursor-pointer"
+          >확인</button>
+        </div>
+      </form>
+    </ModalCover>
+  )
+}
+
+export default LinkModal
