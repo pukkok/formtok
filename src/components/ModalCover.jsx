0727a6bd4eb4a54f08d3dd4290f0604b96b56e0b
Pukkok <krystalgggg@gmail.com>
2025-06-23 20:40:06 +0900

    [Fix] 모달커버 z-index 위치 변경

--- src/components/ModalCover.jsx
+++ src/components/ModalCover.jsx
@@ -1,8 +1,8 @@
 const ModalCover = ({ children }) => {
 
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-black/20">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-1000">
+    <div className="fixed top-0 left-0 w-screen h-screen bg-black/20 z-1000">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
         {children}
       </div>
     </div>
