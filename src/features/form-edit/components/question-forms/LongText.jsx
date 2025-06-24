32da3ecf94d0c00c71694701064e418f627955da
Pukkok <krystalgggg@gmail.com>
2025-06-24 17:11:46 +0900

    [Design] 질문 폼 마진제거

--- src/features/form-edit/components/question-forms/LongText.jsx
+++ src/features/form-edit/components/question-forms/LongText.jsx
@@ -1,6 +1,6 @@
 /** 서술형 */
 const LongText = () => (
-  <div className="mt-4 w-full h-20 py-2 px-2.5 rounded-xl bg-light-w">
+  <div className="w-full h-20 py-2 px-2.5 rounded-xl bg-light-w">
     <input placeholder="서술형 (1000자 이내)" disabled={true}/>
   </div>
 )
--- src/features/form-edit/components/question-forms/ShortText.jsx
+++ src/features/form-edit/components/question-forms/ShortText.jsx
@@ -1,7 +1,7 @@
 /** 단답형 */ 
 function ShortText () {
   return  (
-    <div className="mt-4 w-4/10 h-10 px-2.5 py-2 rounded-xl bg-light-w">
+    <div className="w-4/10 h-10 px-2.5 py-2 rounded-xl bg-light-w">
       <input placeholder="단답형 (100자 이내)" disabled={true}/>
     </div>
   )
