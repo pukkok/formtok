c50c2539804ba7b2dbddf4fe42dea028ebf3960e
Pukkok <krystalgggg@gmail.com>
2025-06-26 01:31:37 +0900

    [Feat] 질문 접은 경우 추가

--- src/stores/useFormEditUiStore.js
+++ src/stores/useFormEditUiStore.js
@@ -1,9 +1,15 @@
 import { create } from 'zustand'
 
 export const useFormEditUiStore = create((set, get) => ({
-  activeCard: '',
-  setActiveCard: (value) => set({activeCard : value}) 
-  
-  
+  activeCard: 'P-0',
+  setActiveCard: (value) => set({activeCard : value}),
+
+  foldQuestions: [],
+  toggleFoldQuestion: (value) => {
+    const foldQuestions = get().foldQuestions
+    foldQuestions.includes(value) ?
+    set({foldQuestions : foldQuestions.filter(q => q !== value)}) :
+    set({foldQuestions : [...foldQuestions, value]})
+  },
 
 }))
