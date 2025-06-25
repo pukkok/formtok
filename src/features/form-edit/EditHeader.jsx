d520c9adcddd0b664ecb8eb0e354d0691dc6506b
Pukkok <krystalgggg@gmail.com>
2025-06-25 15:15:36 +0900

    [Feat] 에디터 헤더 생성

---
+++ src/features/form-edit/EditHeader.jsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useEffect, useRef } from "react"
+import { useFormEditStore } from "@/stores/useFormEditStore"
+import { useParams, useRouter } from "next/navigation"
+import MoveToLoginPageModal from "./components/MoveToLoginPageModal"
+import ModalContainer from "@/components/ModalContainer"
+import { useAuthStore } from "@/stores/useAuthStore"
+
+const EditHeader = () => {
+    const { id } = useParams()
+    const router = useRouter()
+    
+    const userInfo = useAuthStore(s => s.userInfo)
+
+    const title = useFormEditStore(s => s.title)
+    const setTitle = useFormEditStore(s => s.setTitle)
+
+    const loadForm = useFormEditStore(s => s.loadForm)
+    const saveFormAction = useFormEditStore(s => s.saveFormAction)
+
+    const moveToLoginPageModalRef = useRef(null)
+
+    const settingOriginData = useFormEditStore(s => s.settingOriginData)
+
+    useEffect(() => {
+      if(!userInfo) return settingOriginData()
+      loadForm(id)
+  }, [loadForm, userInfo, settingOriginData])
+
+    const authCheckAndSaveForm = async (url) => {
+      if(!userInfo) moveToLoginPageModalRef.current?.open()
+      else await saveFormAction(url)
+    }
+
+    return (
+      <header className={`sticky top-0 w-full flex items-center h-15 px-5 z-300
+      dark:bg-deep-dark bg-bright-a
+      border-b dark:border-b-charcoal border-b-gray-300
+      `}>
+          <input 
+            className="w-xs text-lg mr-auto pl-2 p-1 
+            overflow-hidden text-ellipsis whitespace-nowrap
+            border-2 border-transparent focus:border-point rounded-xl outline-none"
+            onChange={e =>setTitle(e.target.value)} placeholder="제목없는 설문지" value={title}/>
+          <button 
+            className="bg-point hover:bg-point-hover ml-2.5 px-2.5 py-1.5 font-bold text-light-w rounded-md cursor-pointer"
+            onClick={()=>router.push(`/my-form/preview/${id}`)}>미리보기</button>
+          <button 
+            className="bg-point hover:bg-point-hover ml-2.5 px-2.5 py-1.5 font-bold text-light-w rounded-md cursor-pointer"
+            onClick={() => authCheckAndSaveForm(id)}>저장</button>        
+
+          <ModalContainer ref={moveToLoginPageModalRef}>
+            <MoveToLoginPageModal onConfirm={() => router.push('/login')} />
+          </ModalContainer>
+      </header>
+    )
+}
+
+export default EditHeader
