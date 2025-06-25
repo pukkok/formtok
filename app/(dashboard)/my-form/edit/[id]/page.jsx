638f6064869c396adbfd08f0b2e0009f4191fb79
Pukkok <krystalgggg@gmail.com>
2025-06-26 01:37:11 +0900

    [Feat] 에딧페이지 초기 세팅 추가 및 스타일링 수정

--- app/(dashboard)/my-form/edit/[id]/page.jsx
+++ app/(dashboard)/my-form/edit/[id]/page.jsx
@@ -3,19 +3,25 @@
 import EditHeader from "@/features/form-edit/EditHeader"
 import EditSidebar from "@/features/form-edit/EditSidebar"
 import FormEditor from "@/features/form-edit/FormEditor"
+import { useFormEditUiStore } from "@/stores/useFormEditUiStore"
 import { useEffect } from "react"
 
 const EditPage = () => {
 
+  const setActiveCard = useFormEditUiStore(s => s.setActiveCard)
+
   useEffect(() => {
     window.history.pushState(null, '', window.location.href)
+    setActiveCard('P-0')
   }, [])
 
   return (
     <div className="flex justify-start">
       <div className="flex-1">
         <EditHeader />
-        <FormEditor />
+        <div className="overflow-scroll h-[calc(100vh-60px)] mx-1">
+          <FormEditor />
+        </div>
       </div>
       <div className="shrink-0 ml-auto">
         <EditSidebar />
