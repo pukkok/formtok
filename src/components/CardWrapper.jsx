0e864a300339e66ff94fbd73bc261494154412f8
Pukkok <krystalgggg@gmail.com>
2025-06-27 19:54:44 +0900

    [File] 공통스타일 커버 components로 이동

--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -6,7 +6,7 @@ const CardWrapper = ({ children }) => {
       shadow-md
       transition-[box-shadow_300ms_cubic-bezier(0.4,0,0.2,1)_0ms]
       overflow-hidden rounded-xl
-      dark:bg-dark cursor-pointer
+      cursor-pointer
     `}>
       {children}
     </div>
