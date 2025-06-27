3721d73211da377d23a4754f36e3def967a37932
Pukkok <krystalgggg@gmail.com>
2025-06-27 22:39:50 +0900

    [Fix] morevert버튼 색상 머지를 위한 조건 변경

--- src/features/form-edit/components/right-sidebar/FormSummary/MoreButton.jsx
+++ src/features/form-edit/components/right-sidebar/FormSummary/MoreButton.jsx
@@ -3,7 +3,7 @@ const MoreButton = ({className, onClick, children, ...props}) => {
   return (
     <button
     onClick={onClick}
-    className={`dark:hover:bg-dark-elevated dark:text-bright-a text-dark-deep hover:bg-light-w px-2 py-2 rounded-md ${className}`}
+    className={`dark:hover:bg-dark-elevated hover:bg-light-w px-2 py-2 rounded-md ${className || 'dark:text-bright-a text-dark-deep'}`}
     {...props}
     >{children}</button>
   )
