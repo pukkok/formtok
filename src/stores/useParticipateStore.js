344211a8922d395fc8df50f6bf3b08b6031ab5d8
Pukkok <krystalgggg@gmail.com>
2025-06-27 22:42:49 +0900

    [Feat] 참여하기 상태관리 생성

---
+++ src/stores/useParticipateStore.js
@@ -0,0 +1,22 @@
+import { getAvailableForms } from '@/apis/forms'
+import { safeRequest } from '@/utils/safeRequest'
+import { create } from 'zustand'
+
+export const useParticipateStore = create((set, get) => ({
+  
+  allForms: [],
+
+  searchedForms: [],
+  setSearchedForms: (arr) => set({ searchedForms: arr }),
+
+  getAvailableFormsAction: async () => {
+    const { result } = await safeRequest(getAvailableForms(), {
+      loadingMessage: '종이 줍는 중..'
+    })
+
+    if(result) {
+      set({allForms: result.forms, searchedForms: result.forms})
+    }
+  }
+
+}))
