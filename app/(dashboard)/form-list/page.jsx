87c1e98d7e9e5dad4f559a463bd24574c830c541
Pukkok <krystalgggg@gmail.com>
2025-06-27 19:55:46 +0900

    [File] 설문 참여 페이지명 변경

--- app/(dashboard)/form-list/page.jsx
+++
@@ -1,8 +0,0 @@
-const FormListPage = () => {
-  
-  return (
-    <div>폼 리스트 페이지</div>
-  )
-}
-
-export default FormListPage
---
+++ app/(dashboard)/participate/page.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import ParticipateHeader from "@/features/participate/ParticipateHeader"
+import ParticipateTemplateBox from "@/features/participate/ParticipateTemplateBox"
+
+const ParticipatePage = () => {
+  
+  return (
+    <div>
+      <ParticipateHeader />
+      <ParticipateTemplateBox />
+    </div>
+  )
+}
+
+export default ParticipatePage
