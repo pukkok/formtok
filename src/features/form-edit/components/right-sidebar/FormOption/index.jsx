ccd9634e37e374190c577f5aa9323a4f6d325cc9
Pukkok <krystalgggg@gmail.com>
2025-06-29 23:48:16 +0900

    [Fix] 에디터 옵션탭 선택부분 하단 여백 추가

--- src/features/form-edit/components/right-sidebar/FormOption/index.jsx
+++ src/features/form-edit/components/right-sidebar/FormOption/index.jsx
@@ -69,7 +69,7 @@ export const PARTICIPANT_PERMISSIONS_OPTIONS = [
 const FormOption = () => {
 
   return (
-    <div className="w-full h-[calc(100vh-60px)] overflow-scroll box-border py-2.5 pl-4 pr-6">
+    <div className="w-full h-[calc(100vh-60px)] overflow-scroll box-border pt-2.5 pl-4 pr-6 pb-8">
       <FormListStyleSelector />
 
       <Head4>설문지 관리</Head4>
