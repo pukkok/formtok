00fc525af9588754f80faa29c45b94084f0523ee
Pukkok <krystalgggg@gmail.com>
2025-06-29 22:50:42 +0900

    [Feat] 답변 api 추가

---
+++ src/apis/answers.js
@@ -0,0 +1,7 @@
+import authAxios from "@/utils/authAxios"
+import axios from "axios"
+
+export const submitAnswer = async (url, answers) => {
+  const { data } = await authAxios.post(`/api/answer/submit/?url=${url}`, {answers})
+  return data
+}
