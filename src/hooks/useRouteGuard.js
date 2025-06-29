8f6ee13e35f3557c1471c90d4500bf430f874f17
Pukkok <krystalgggg@gmail.com>
2025-06-29 17:44:20 +0900

    [Hotfix] 팝스테이트 정상작동하도록 변경

--- src/hooks/useRouteGuard.js
+++ src/hooks/useRouteGuard.js
@@ -53,8 +53,8 @@ const useRouteGuard = () => {
 
   useEffect(() => {
     const onPopState = () => {
-      // TODO: 에딧페이지가 아니면 팝스테이트는 무시해야한다.
-      if(!pathname.includes('/my-form/edit')) return
+      // TODO: edit페이지의 이전 pathname은 /my-form/manage이므로 /my-form/manage가 아니면 팝스테이트는 무시해야한다.
+      if(!pathname.includes('/my-form/manage')) return console.log('포함하지 않아')
 
       if (!isModified()) return router.push('/my-form/manage')
       modalRef.current?.open()
