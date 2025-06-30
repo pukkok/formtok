c6ec397fc49c7eab736d59c138b5732505662ef6
Pukkok <krystalgggg@gmail.com>
2025-07-01 01:06:06 +0900

    [Feat] 워크컬러 텍스트 색상 추가

--- src/utils/workColor.js
+++ src/utils/workColor.js
@@ -3,6 +3,7 @@ const PARTICIPATE_KEYS = ['all', 'faq', 'noLogin', 'active', 'finish']
 const QUESTION_BANK_KEYS = ['all', 'choice', 'dropdown', 'datetime', 'table', 'text', 'score']
 const workColors = ['bg-point', 'bg-work-sky', 'bg-work-yellow', 'bg-work-green', 'bg-work-red', 'bg-work-gray', 'bg-work-orange']
 const hoverWorkColors = ['hover:bg-point', 'hover:bg-work-sky', 'hover:bg-work-yellow', 'hover:bg-work-green', 'hover:bg-work-red', 'hover:bg-work-gray', 'hover:bg-work-orange']
+const textWorkColors = ['text-point', 'text-work-sky', 'text-work-yellow', 'text-work-green', 'text-work-red', 'text-work-gray', 'text-work-orange']
 
 export const MANAGE_WORK_COLOR_MAP = Object.fromEntries(
   MANAGE_KEYS.map((key, i) => [key, workColors[i]])
@@ -26,4 +27,8 @@ export const QUESTION_BANK_COLOR_MAP = Object.fromEntries(
 
 export const QUESTION_BANK_HOVER_COLOR_MAP = Object.fromEntries(
   QUESTION_BANK_KEYS.map((key, i) => [key, hoverWorkColors[i]])
+)
+
+export const QUESTION_BANK_TEXT_COLOR_MAP = Object.fromEntries(
+  QUESTION_BANK_KEYS.map((key, i) => [key, textWorkColors[i]])
 )
