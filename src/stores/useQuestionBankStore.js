a32acab0a64ff1e652462a943d22e6a1e803dce5
Pukkok <krystalgggg@gmail.com>
2025-07-01 13:05:12 +0900

    [Feat] 문항 삭제기능 및 선택 리셋기능 추가

--- src/stores/useQuestionBankStore.js
+++ src/stores/useQuestionBankStore.js
@@ -1,5 +1,6 @@
-import { getAllMyQuestions } from "@/apis/questions"
+import { deleteManyQuestions, deleteQuesiton, getAllMyQuestions } from "@/apis/questions"
 import { safeRequest } from "@/utils/safeRequest"
+import { toast } from "sonner"
 import { create } from "zustand"
 
 export const useQuestionBankStore = create((set, get) => ({
@@ -15,14 +16,41 @@ export const useQuestionBankStore = create((set, get) => ({
     set({ selectedQuestions: [...selectedQuestions.filter(qId => qId !== id)] }) :
     set({ selectedQuestions: [...selectedQuestions, id]})
   },
+  resetSelectedQuestions: () => set({selectedQuestions : []}),
 
   loadQuestions: async () => {
     const { result } = await safeRequest(getAllMyQuestions(), {
-      successMessage: '설문지 리스트 다가져옴'
+      
     })
 
     if(result) {
       set({questions : result.questions, searchedQuestions: result.questions})
+    }
+  },
+
+  deleteQuestionAction: async (id) => {
+    const { result } = await safeRequest(deleteQuesiton(id), {
+      successMessage: '문항이 삭제되었습니다.',
+      successDuration: 2500
+    })
+    if(result) {
+      get().loadQuestions()
+    }
+  },
+
+  deleteManyQuestionsAction: async () => {
+    const { selectedQuestions } = get()
+
+    if(selectedQuestions.length === 0) return toast.error('선택된 문항이 없습니다.')
+    
+    const { result } = await safeRequest(deleteManyQuestions(selectedQuestions), {
+      successMessage: '문항이 삭제되었습니다.',
+      successDuration: 2500
+    })
+
+    if(result) {
+      set({ selectedQuestions: [] })
+      get().loadQuestions()
     }
   }
 
