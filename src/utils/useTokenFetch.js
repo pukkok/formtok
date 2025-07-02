23d995ab3e972fbaacef8d444ef5e7ab318883c0
Pukkok <krystalgggg@gmail.com>
2025-07-03 02:27:24 +0900

    [Feat] useTokenFetch 옵션 추가

--- src/utils/useTokenFetch.js
+++ src/utils/useTokenFetch.js
@@ -1,10 +1,10 @@
 import { useEffect } from "react"
 import { useAuthStore } from "@/stores/useAuthStore"
 
-export const useTokenFetch = (fetchAction) => {
+export const useTokenFetch = (fetchAction, option) => {
   const token = useAuthStore(s => s.token)
 
   useEffect(() => {
-    if(token) fetchAction(true)
+    if(token) fetchAction(true, option)
   }, [token])
 }
